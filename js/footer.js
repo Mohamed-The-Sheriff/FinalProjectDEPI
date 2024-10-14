2ba2a39084cd77dc049580834030392ac912757c
Mohamed-The-Sheriff <160542866+Mohamed-The-Sheriff@users.noreply.github.com>
2024-10-15 01:13:19 +0300

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,55 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `
+    <div class="footer-content">
+        <img src="img/unnamed.png" class="logo" alt="">
+        <div class="footer-ul-container">
+            <ul class="category">
+                <li class="category-title">men</li>
+                <li><a href="#" class="footer-link">t-shirts</a></li>
+                <li><a href="#" class="footer-link">sweatshirts</a></li>
+                <li><a href="#" class="footer-link">shirts</a></li>
+                <li><a href="#" class="footer-link">jeans</a></li>
+                <li><a href="#" class="footer-link">trousers</a></li>
+                <li><a href="#" class="footer-link">shoes</a></li>
+                <li><a href="#" class="footer-link">casuals</a></li>
+                <li><a href="#" class="footer-link">formals</a></li>
+                <li><a href="#" class="footer-link">sports</a></li>
+                <li><a href="#" class="footer-link">watch</a></li>
+            </ul>
+            <ul class="category">
+                <li class="category-title">women</li>
+                <li><a href="#" class="footer-link">t-shirts</a></li>
+                <li><a href="#" class="footer-link">sweatshirts</a></li>
+                <li><a href="#" class="footer-link">shirts</a></li>
+                <li><a href="#" class="footer-link">jeans</a></li>
+                <li><a href="#" class="footer-link">trousers</a></li>
+                <li><a href="#" class="footer-link">shoes</a></li>
+                <li><a href="#" class="footer-link">casuals</a></li>
+                <li><a href="#" class="footer-link">formals</a></li>
+                <li><a href="#" class="footer-link">sports</a></li>
+                <li><a href="#" class="footer-link">watch</a></li>
+            </ul>
+        </div>
+    </div>
+    <p class="footer-title">about company</p>
+    <p class="info">We at [Okaz Shopping Mall] strive to provide the best online shopping experience to our valued customers. We offer you a wide range of clothes and accessories to suit all tastes and occasions. Whether you're looking for the latest fashion trends or must-have classics, you'll find everything you need here.</p>
+    <p class="info">support emails - MAG@clothing.com, customersupport@clothing.com</p>
+    <p class="info">telephone - 180 00 00 001, 180 00 00 002</p>
+    <div class="footer-social-container">
+        <div>
+            <a href="#" class="social-link">terms & services</a>
+            <a href="#" class="social-link">privacy page</a>
+        </div>
+        <div>
+            <a href="#" class="social-link">instagram</a>
+            <a href="#" class="social-link">facebook</a>
+            <a href="#" class="social-link">twitter</a>
+        </div>
+    </div>
+    <p class="footer-credit">Clothing, Best apparels online store</p>
+    `;
+}
+
+createFooter();
